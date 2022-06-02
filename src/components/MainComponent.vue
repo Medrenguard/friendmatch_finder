6b8ddb1a03bc767d51d62472ff1706adb83147f7
Medrenguard <0892364395@mail.ru>
2022-06-02 20:10:24 +0300

    first commit

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    hello world!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComponent',
+  data () {
+    return {
+      testData: undefined
+    }
+  }
+}
+</script>
