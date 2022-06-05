ff948463ff32eadb2d1379712f8073e10c839efc
Medrenguard <0892364395@mail.ru>
2022-06-05 13:09:26 +0300

    Добавлен шаблон для страницы друга

---
+++ src/views/Friend.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        hello, friend!
+    </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  name: 'Friend',
+  created () {
+    console.log()
+  }
+}
+
+</script>
