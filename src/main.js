9da4c8aa01ed406770cffdc70f300643ab00cdf5
Medrenguard <0892364395@mail.ru>
2022-09-25 12:37:07 +0300

    Добавляет библиотеку уведомлений

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App'
 import PerfectScrollbar from 'vue2-perfect-scrollbar'
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
 import {store} from '../store'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+const toastOptions = {
+  // You can set your default options here
+}
 
 Vue.use(PerfectScrollbar)
+Vue.use(Toast, toastOptions)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
