0ff9ae4f451f8230500526c6a8c78008c6be16a5
Nick <0892364395@mail.ru>
2025-05-14 20:32:57 +0300

    Правит ссылки публикации 2

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: '/friendmatch_finder/',
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].base = '/friendmatch_finder/';
-      return args;
-    });
-  }
-};
